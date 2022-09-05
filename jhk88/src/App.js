0a6872d86711597cdb19bce4fb0b82d93541f285
김 지혜 <jhk88@jinhaksa.net>
2022-09-05 16:34:31 +0900

    feat. phone layout

---
+++ jhk88/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className='h-screen flex'>
+      <div className='mockup-phone'>
+        <div className='camera'></div>
+        <div className='display'>
+          <div className='artboard artboard-demo phone-1'></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
