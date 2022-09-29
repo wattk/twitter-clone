88a5c4c07df6c4a656caa8a18e929e833aa84130
Hyejin Kwon <91240732+wattk@users.noreply.github.com>
2022-09-29 11:02:39 +0900

    Delete jhk88 directory

--- jhk88/src/data/state.js
+++
@@ -1,8 +0,0 @@
-export const profile = 'PROFILE';
-export const search = 'SEARCH';
-export const mail = 'MAIL';
-export const home = 'HOME';
-export const mic = 'MIC';
-export const alarm = 'ALARM';
-export const write_tweet = 1;
-export const write_mail = 2;
